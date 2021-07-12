d14f92e34f3d5f3648d96fcdb1fac45ab9843408
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2021-07-12 13:52:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: "User not exists!" })
+  }
+
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const todoLength = user.todos.length
+
+  if(user.pro === false && todoLength >= 10) {
+    return response.status(403).json({ error: "" })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: "User not exists!" })
+  }
+
+  const isUUID = validate(id)
+
+  if(isUUID === false) {
+    return response.status(400).json({ error: "ID is not UUID!" })
+  }
+
+  const todo = user.todos.find(user => user.id === id)
+
+  if(!todo) {
+    return response.status(404).json({ error: "ID invalid!" })
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: "User not exists!" })
+  }
+  
+  const idExist = users.find(user => user.id)
+
+  if(id !== idExist.id) {
+    return response.status(404).json({ error: "ID not found!" })
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
